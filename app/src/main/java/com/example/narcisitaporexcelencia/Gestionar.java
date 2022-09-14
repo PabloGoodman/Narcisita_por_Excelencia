e7f8077e01dc51c7be8718b135a28e3ad5f9c1e5
Pablo <pblealrogel@gmail.com>
2022-09-14 10:50:12 -0300

    Nuevos botones y labels.

---
+++ app/src/main/java/com/example/narcisitaporexcelencia/Gestionar.java
@@ -0,0 +1,14 @@
+package com.example.narcisitaporexcelencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gestionar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionar);
+    }
+}
--- app/src/main/java/com/example/narcisitaporexcelencia/MainActivity.java
+++ app/src/main/java/com/example/narcisitaporexcelencia/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.narcisitaporexcelencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(MainActivity.this, Gestionar.class);
+        startActivity(i);
+    }
 }
+
